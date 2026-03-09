40cd53ac499e49609613857cf4ed8f6abc7a2ba9
Dhruv-Omnenest <Dhruv.mehta@omnenest.com>
2026-03-09 11:37:48 +0530

    file name

--- client/src/pages/SplashScreen.tsx
+++ client/src/pages/SplashScreen.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
-import { useAuthStore } from "@/store/auth.store";
-import { preAuthHandshake } from "@/services/api/auth/AuthService";
+import { useAuthStore } from "../store/auth.store";
+import { preAuthHandshake } from "../services/api/auth/AuthService";
 
 
 export const SplashScreen: React.FC = () => {
